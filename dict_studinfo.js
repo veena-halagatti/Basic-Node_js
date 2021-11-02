fcd1e7ce9d9403ecae815cc96f69b349e7242d5a
veena-halagatti <92357922+veena-halagatti@users.noreply.github.com>
2021-11-02 15:16:00 +0530

    Update dict_studinfo.js

--- dict_studinfo.js
+++ dict_studinfo.js
@@ -1,7 +1,7 @@
 var person = {
-    firstName: "Jyothi",
-    lastName: "M",
-    USN: 59,
+    firstName: "VEENA",
+    lastName: "H",
+    USN: 30,
     marks1: 85,
     marks2: 80,
     marks3: 90
